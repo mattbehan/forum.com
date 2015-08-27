b24210256ac3a7f2b6c0e30bd74be366f20f53b6
Cat McLoughlin & Matt Behan & Nathan Vanderlaan &   "name": null <pair+catmclough.mattbehan.nvanderlaan.pbsugg@devbootcamp.com>
2015-08-27 13:12:46 -0500

    creating vote branch with info still only in answer and user models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,4 +6,50 @@ class Answer < ActiveRecord::Base
   has_many :votes, :as => :votable
 
   validates :body, presence: true
+
+  #called from user
+  #INPUT: Value is an integer representing upvote/downvote, votable is the OBJECT that the user is voting on. called as user.cast_vote
+  # Output: no usable output. creates a vote object.
+  def cast_vote (value, votable)
+    #need to change class to string
+    new_vote = Vote.create(value: value, votable_id: votable.id,votable_type: votable.class.to_s, user_id: self.user_id)
+    self.votes << new_vote
+  end
+
+  def reputation_from_posted_questions_and_answers
+    Vote.where(user_id: self.id).score_from_votes
+  end
+
+  # input
+  def has_user_voted_on_this_before? user_id
+    Vote.find_by(value: value, votable_type: self.class, user_id: user_id)
+  end
+
+
+  # this needs to be called in questions
+  def highest_voted_answers
+    self.answers.sort {|answer| answer.vote_score}
+  end
+
+  #to be called as question.vote_score/ answer.vote_score
+  def vote_score
+    self.votes.score_from_votes
+  end
+
+  def score_from_votes
+    self.reduce(0, :+){|sum, vote| sum + vote.value}
+  end
+
+  # x=0 10.times do Vote.(value: rand(2), votable_type: "answer", user_id=x  ) x +=1 end
+
 end
+
+# answer.vote_score
+#question.vote_score
+#question.highest_voted_answer
+#answers.highest_voted
+#answer.cast_vote
+#question.cast_vote
+#only be able to vote on question/answer once per user
+
+
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,19 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, :polymorphic => true
 
   validates :votable_type, presence: true
+  validates :value, presence: true
+
+
+
 
 end
+
+
+# answer.vote_score
+#question.vote_score
+#question.highest_voted_answer
+#answers.highest_voted
+#answer.cast_vote
+#question.cast_vote
+#only be able to vote on question/answer once per user
+Vote.create
