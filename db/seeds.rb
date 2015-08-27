754228a022e506baf82513e7ab865b169191556d
Cat McLoughlin & Nathan Vanderlaan <pair+catmclough.nvanderlaan@devbootcamp.com>
2015-08-27 14:49:54 -0500

    Seed answers and betterfy the question seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,12 @@ module Seed
       location = Faker::Address.city
       User.create(email: email, display_name: display_name, password: pword, location: location)
 
-      lorem_title = Faker::Lorem.sentence
-      lorem_body = Faker::Lorem.paragraph
-      Question.create(title: lorem_title, body: lorem_body)
+      question_title = (Faker::Lorem.sentence).chomp('.') + "?"
+      question_body = Faker::Lorem.paragraph(7)
+      Question.create(title: question_title, body: question_body)
+
+      answer_body = Faker::Lorem.paragraph(3)
+      Answer.create(body: answer_body)
     end
   end
 
