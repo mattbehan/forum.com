53b2fea468df740278c91e01296d550aaab819a4
Cat McLoughlin & Matt Behan & Nathan Vanderlaan &   "name": null <pair+catmclough.mattbehan.nvanderlaan.pbsugg@devbootcamp.com>
2015-08-27 15:52:16 -0500

    finished up vote methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,20 @@ module Seed
       answer_body = Faker::Lorem.paragraph(3)
       Answer.create(body: answer_body)
     end
+
+    User.first.cast_vote(1,Answer.first)
+    User.second.cast_vote(1,Answer.first)
+    User.third.cast_vote(1,Answer.first)
+    User.fourth.cast_vote(1,Answer.first)
+    User.fifth.cast_vote(1,Answer.first)
+    User.first.cast_vote(1,Question.first)
+    User.second.cast_vote(1,Question.first)
+    User.third.cast_vote(1,Question.first)
+    User.fourth.cast_vote(1,Question.first)
+    User.fifth.cast_vote(1,Question.first)
+
+
+
   end
 
 end
